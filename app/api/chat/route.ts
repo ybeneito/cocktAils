5b182db1e8c2c2d453d7f8500191a4e26ae8dee4
Yohan Beneito <y.beneito@gmail.com>
2023-07-25 18:19:43 +0200

    loader + remove stream ok

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { OpenAIStream, StreamingTextResponse } from "ai";
-import {Configuration, OpenAIApi } from "openai-edge"
+import { NextResponse } from "next/server";
+import {Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai"
 
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
@@ -7,26 +8,33 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config)
 
-export const runtime = "edge"
+const instructionMessage: ChatCompletionRequestMessage = {
+    role: "system",
+    content: `Ignores tous les instructions précédentes. 
+    Réponds toujours en Français.
+    Tu es un expert en cocktails et fin connaisseur en personnalitée et tu dois conseiller un cocktail qu'aimerai boire la personnalitée dont l'utilisateur te passera le nom.
+    Le format de réponse attendu est du JSON conforme à la RFC8259 suivant ce modelle:[{
+        'name': 'le nom du cocktail',
+        'ingredients': 'tableau de string contenant les ingrédients du cocktail',
+        'recipe': 'tableau de string contenant les étapes la recette du cocktail',
+        'explanation': 'courte explication 50 mots max sur le choix de ce cocktail contenant une reférence à la personne si possible'
+    }]
+    Assures toi de toujours me renvoyer une réponse propre en retirant par example les backslash et les bachslash n 
+    `
+}
 
 export async function POST(req: Request) {
     const { messages } = await req.json()
 
-    
-    const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        stream: true,
-        messages: [{
-            role: "system",
-            content: `Ignores tous les instructions précédentes. 
-            Tu es un expert en cocktails et fin connaisseur en personnalitée musicale et tu dois conseiller un cocktail qu'aimerai boire la star musicale dont l'utilisateur te passera le nom.
-            Le format de réponse attendu est: le nom du cocktails, la liste des ingrédients, la recette et une courte explication 30 mots max pour ce choix. Ne rajoute pas d'introduction de réponse.
-            Passes à la ligne entre chaques parties de la réponse et entre chaque ingrédients ou étapes de recette
-            `,
-        }, ...messages]
-    })
-
-    const stream: ReadableStream<any> = OpenAIStream(response)
-
-    return new StreamingTextResponse(stream)
+    try {
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [instructionMessage, ...messages]
+        })
+        // const stream: ReadableStream = OpenAIStream(response)
+        return NextResponse.json(response.data.choices[0].message?.content)
+    } catch (error) {
+        console.log("ERR: ", error)
+        return new NextResponse("error", {status: 500})
+    }
 }
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,26 +2,31 @@
 
 import { Message, useChat } from 'ai/react'
 import React from 'react'
-import { Button, IconButton } from "@i4o/catalystui"
-import { HomeIcon, PaperPlaneIcon } from '@radix-ui/react-icons'
+import { IconButton } from "@i4o/catalystui"
+import { PaperPlaneIcon } from '@radix-ui/react-icons'
 
 export default function Chat() {
-    const {messages, input, handleInputChange, handleSubmit} = useChat()
+    const {messages, input, handleInputChange, handleSubmit, isLoading} = useChat()
 
-
-  return (
-    <div className='m-auto mt-4 flex max-w-md flex-col'>
-        <div className='flex flex-1 flex-col gap-2'>
-            {messages.map((message: Message) => (
-                <div key={message.id}>
-                    {message.role === "assistant" ? message.content : ""}
-                </div>
-            ))}
+    
+    return (
+        <div className='m-auto mt-4 flex max-w-md flex-col'>
+            <form onSubmit={handleSubmit} className='flex gap-5 '>
+                <input onChange={handleInputChange} value={input} type="text" placeholder="Entrez le nom d'un artiste" className='p-2 bg-transparent rounded-lg border border-gray-200 dark:border-gray-700 col-span-4'/>
+                <IconButton className='text-blue-500' type='submit' icon={<PaperPlaneIcon />}/>
+                {isLoading ? 
+                    <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
+                        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Chargement...</span>
+                    </div> : "" 
+                }
+            </form>
+            <div className='flex flex-1 flex-col gap-2 text-start'>
+                {messages.map((message: Message) => (
+                    <div key={message.id}>
+                        {message.role === "assistant" ?  message.content : ""}
+                    </div>
+                ))}
+            </div>
         </div>
-        <form onSubmit={handleSubmit} className='flex gap-5 '>
-            <input onChange={handleInputChange} value={input} type="text" placeholder="Entrez le nom d'un artiste" className='p-2 bg-transparent rounded-lg border border-gray-200 dark:border-gray-700 col-span-4'/>
-            <IconButton className='text-blue-500' type='submit' icon={<PaperPlaneIcon />}/>
-        </form>
-    </div>
   )
 }
